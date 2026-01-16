e51fe357ee6edfe5bfa8a6b5f4e0926f1a5939ee
sssiion <rta2752775@gmail.com>
2026-01-16 10:12:19 +0900

    깃허브 페이지 배포 오류
    절대경로 / > 상대경로 ./로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project_SYNCLIFE/', // GitHub Pages repository name
+  base: './', // Use relative paths for flexibility
 })
